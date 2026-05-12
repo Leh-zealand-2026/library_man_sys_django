20021abfe593ed9ba6abda4219197f67d7941eec
Leh-zealand-2026 <leh001@edu.zealand.dk>
2026-05-12 07:51:20 +0200

    phase 1: adding form to views.py

--- books/views.py
+++ books/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import RegisterForm
+from .models import Member
+
+
+# https://docs.djangoproject.com/en/6.0/topics/auth/default/#built-in-auth-views
+
+def home(request):
+
+    return render(request, "books/home.html")
+
+
+# member registration.
+def register(request):
+    # we created RegisterForm in forms.py
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            
+            user = form.save()
+            Member.objects.create(user=user)
+
+            login(request, user)
+            return redirect("home")
+    else:
+        form = RegisterForm()
+
+    return render(request, "books/register.html", {"form": form})
