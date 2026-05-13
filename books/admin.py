dfd3b7fd5ebfc94482f051828b7362d6870eacec
Leh-zealand-2026 <leh001@edu.zealand.dk>
2026-05-13 07:11:25 +0200

    added models to admin.py

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Book, BorrowRecord, Category, Member
+
+
+admin.site.register(Category)
+admin.site.register(Book)
+admin.site.register(Member)
+admin.site.register(BorrowRecord)
